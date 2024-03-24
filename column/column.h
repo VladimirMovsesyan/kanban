241fafb56546b7fff396b7562dc1248195c6d495
vladimirmovsesyan <vladimirmovsesyan-dev@yandex.ru>
2024-03-24 23:54:44 +0300

    Columns and tasks base added.

---
+++ column/column.h
@@ -0,0 +1,28 @@
+#ifndef KANBAN_COLUMN_H
+#define KANBAN_COLUMN_H
+#include "../task/task.h"
+#include <iostream>
+#include <vector>
+class Column {
+private:
+    std::string name;
+    int color;
+    std::vector<Task> tasks;
+
+public:
+    Column();
+    Column(const std::string &, int, int, int);
+
+    ~Column() = default;
+
+    void SetName(const std::string &);
+    std::string GetName();
+    void SetColor(int, int, int);
+    void SetColor(int);
+    int GetColor() const;
+
+    void SetTasks(std::vector<Task>);
+    std::vector<Task> GetTasks();
+};
+
+#endif//KANBAN_COLUMN_H
